28e756924339872339c3a798590df2e1cd989b24
taurusilver7 <prasad20697@gmail.com>
2021-02-08 13:02:46 +0530

    requests file added

---
+++ src/Requests.js
@@ -0,0 +1,17 @@
+// f74ccdd76a7932d4705499b7725a338c
+// Typically it'd be better store in {process.env.API_KEY}
+
+const API_KEY = "f74ccdd76a7932d4705499b7725a338c";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&languages=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
