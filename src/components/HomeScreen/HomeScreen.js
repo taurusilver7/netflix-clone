c05968386358e4388a5776ca5d0dedc3bc16839e
taurusilver7 <prasad20697@gmail.com>
2021-02-08 13:04:34 +0530

    homescreen cmp added to project

---
+++ src/components/HomeScreen/HomeScreen.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./HomeScreen.css";
+
+import Navbar from "../Navbar/Navbar";
+import Banner from "../Banner/Banner";
+import Row from "../Row/Row";
+import requests from "../../Requests";
+
+const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+      <Navbar />
+      <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" fetchUrl={requests?.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+};
+
+export default HomeScreen;
