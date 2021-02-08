6ad627e279dee3412b832664468d0c175cd53701
taurusilver7 <prasad20697@gmail.com>
2021-02-08 13:02:13 +0530

    firebase config data added

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5jhajxJhJKZIQ0GNZVc8euoTzUp-FDG8",
+  authDomain: "netflix-clone-e379b.firebaseapp.com",
+  projectId: "netflix-clone-e379b",
+  storageBucket: "netflix-clone-e379b.appspot.com",
+  messagingSenderId: "572714853831",
+  appId: "1:572714853831:web:22c523faa813bdcc5d25e2",
+  measurementId: "G-BQ8H34Y42V",
+};
