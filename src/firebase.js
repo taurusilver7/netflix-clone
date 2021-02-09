1a62166b131e20c0fe1a28ae9c57a48ad9366739
taurusilver7 <prasad20697@gmail.com>
2021-02-09 08:49:51 +0530

    auth linked to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -10,3 +10,17 @@ const firebaseConfig = {
   appId: "1:572714853831:web:22c523faa813bdcc5d25e2",
   measurementId: "G-BQ8H34Y42V",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+// initiate the firebase-app with firebase function above
+
+const db = firebaseApp.firestore();
+// Initiate the database with firestore function
+
+const auth = firebase.auth();
+//  Initiate the authentication with firebase auth fnction.
+
+const storage = firebase.storage();
+// Initiate the storage with firebase storage..
+
+export {db, auth, storage}
